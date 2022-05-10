60bbcf863894e9adca7fb6f8fe1bc359484b137e
viviGarzon <105320528+viviGarzon@users.noreply.github.com>
2022-05-10 16:02:17 -0500

    Add files via upload
    Complemento Muestreo

---
+++ muestreo_modulo/compilar_recursos.bat
@@ -0,0 +1,5 @@
+call "C:\Program Files\QGIS 3.16\bin\o4w_env.bat"
+call "C:\Program Files\QGIS 3.16\bin\qt5_env.bat"
+call "C:\Program Files\QGIS 3.16\bin\py3_env.bat"
+set PATH=%PATH%;C:\Program Files\QGIS 3.16\apps\Python37\Scripts\
+pyrcc5 -o resources.py resources.qrc
